488e03d947e9a40edfcf96b8109b2c0e011ebb7b
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-18 01:36:14 +0000

    headerにリンク作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :profile_image
-  has_many :book, dependent: :destroy
+  has_many :books, dependent: :destroy
 
   def get_profile_imege(width, height)
     unless profile_image.attached?
