dbdb59d83e6c770d4eae06e01d9f75892386efd6
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-21 09:12:39 +0000

    user,index.show/User info部分テンプレート追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,15 +6,12 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+    @user = User.find(current_user.id)
   end
 
   def edit
     @user = User.find(params[:id])
-    if
-      else
-      end
-
-    end
+  
   end
 
   def update
