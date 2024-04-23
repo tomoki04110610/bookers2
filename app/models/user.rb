8cd0769b8a2be2cf4f83718e23d217ee55295029
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-23 12:54:38 +0000

    バリデーション,モデルに記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   has_one_attached :profile_image
   has_many :books, dependent: :destroy
+  validates :name, presence: true
+  validates :introduction, presence: true
 
   def get_profile_image(width, height)
     unless profile_image.attached?
