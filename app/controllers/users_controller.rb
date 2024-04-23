1871b2819c9f2e4a19568143b9743b91bec3fb2b
EC2 Default User <ec2-user@ip-172-31-6-186.ap-northeast-1.compute.internal>
2024-04-23 12:24:07 +0000

    フラッシュメッセージ設定

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,13 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully"
+      redirect_to user_path(@user)
+    else
+      flash.now[:notice] ="error"
+      render :edit
+    end
   end
 end
 
